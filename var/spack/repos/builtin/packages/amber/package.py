eb2d70c866e11566ca916d85fbbd2e3a89b61e9a
Sami Ilvonen <sami.ilvonen@csc.fi>
2021-10-15 15:31:50 +0300

    Complete rewrite of Amber package
    - Moved to CMake and CMakePackage
    - Added gfortran10 patch

--- var/spack/repos/builtin/packages/amber/package.py
+++ var/spack/repos/builtin/packages/amber/package.py
@@ -1,58 +1,67 @@
-# Copyright 2013-2020 Lawrence Livermore National Security, LLC and other
+# Copyright 2013-2021 Lawrence Livermore National Security, LLC and other
 # Spack Project Developers. See the top-level COPYRIGHT file for details.
 #
 # SPDX-License-Identifier: (Apache-2.0 OR MIT)
 
-from spack import *
 import os
 import shutil
 
+from spack import *
 
-class Amber(Package, CudaPackage):
+
+class Amber(CMakePackage, CudaPackage):
     """Amber is a suite of biomolecular simulation programs together
        with Amber tools.
 
-       Note: The version number is composed of the Amber version (major)
-       and the tools version (minor). A manual download is required for
-       both Amber and Amber tools.
-       Spack will search your current directory for the download files.
-       Alternatively, add the files to a mirror so that Spack can find them.
-       For instructions on how to set up a mirror, see
-       http://spack.readthedocs.io/en/latest/mirrors.html"""
+       A manual download is required for Ambers. Spack will search your current
+       directory for the download files. Alternatively, add the files to a mirror
+       so that Spack can find them. For instructions on how to set up a mirror, see
+       https://spack.readthedocs.io/en/latest/mirrors.html
 
-    homepage = "http://ambermd.org/"
+       Note: Only certain versions of ambertools are compatible with amber.
+       Only the latter version of ambertools for each amber version is supported.
+       """
+
+    homepage = "https://ambermd.org/"
     url = "file://{0}/Amber18.tar.bz2".format(os.getcwd())
+    manual_download = True
+
     maintainers = ['hseara']
 
-    def url_for_version(self, version):
-        url = "file://{0}/Amber{1}.tar.bz2".format(
-            os.getcwd(), version.up_to(1))
-        return url
+    version(
+        '20', sha256='a4c53639441c8cc85adee397933d07856cc4a723c82c6bea585cd76c197ead75')
+    version(
+        '18', sha256='2060897c0b11576082d523fb63a51ba701bc7519ff7be3d299d5ec56e8e6e277')
+    version(
+        '16', sha256='3b7ef281fd3c46282a51b6a6deed9ed174a1f6d468002649d84bfc8a2577ae5d',
+        deprecated=True)
 
-    version(
-        '18.20', sha256='2060897c0b11576082d523fb63a51ba701bc7519ff7be3d299d5ec56e8e6e277')
-    version(
-        '18.19', sha256='2060897c0b11576082d523fb63a51ba701bc7519ff7be3d299d5ec56e8e6e277')
-    version(
-        '16.16', sha256='3b7ef281fd3c46282a51b6a6deed9ed174a1f6d468002649d84bfc8a2577ae5d')
-
-    resources = [
+    resources = {
         # [version amber, version ambertools , sha256sum]
-        ('18', '20', 'b1e1f8f277c54e88abc9f590e788bbb2f7a49bcff5e8d8a6eacfaf332a4890f9'),
-        ('18', '19', '0c86937904854b64e4831e047851f504ec45b42e593db4ded92c1bee5973e699'),
-        ('16', '16', '7b876afe566e9dd7eb6a5aa952a955649044360f15c1f5d4d91ba7f41f3105fa'),
-    ]
-    for ver, ambertools_ver, checksum in resources:
-        resource(when='@{0}.{1}'.format(ver, ambertools_ver),
+        '20': ('21', 'f55fa930598d5a8e9749e8a22d1f25cab7fcf911d98570e35365dd7f262aaafd'),
+        '18': ('19', '0c86937904854b64e4831e047851f504ec45b42e593db4ded92c1bee5973e699'),
+        '16': ('16', '7b876afe566e9dd7eb6a5aa952a955649044360f15c1f5d4d91ba7f41f3105fa'),
+    }
+    for ver, (ambertools_ver, ambertools_checksum) in resources.items():
+        resource(when='@{0}'.format(ver),
                  name='AmberTools',
-                 url='file://{0}/AmberTools{1}.tar.bz2'.format(os.getcwd(),
-                                                               ambertools_ver),
-                 sha256=checksum,
+                 url='file://{0}/AmberTools{1}.tar.bz2'.format(
+                     os.getcwd(), ambertools_ver),
+                 sha256=ambertools_checksum,
                  destination='',
                  placement='ambertools_tmpdir',
                  )
 
     patches = [
+        ('20', '1', '10780cb91a022b49ffdd7b1e2bf4a572fa4edb7745f0fc4e5d93b158d6168e42'),
+        ('20', '2', '9c973e3f8f33a271d60787e8862901e8f69e94e7d80cda1695f7fad7bc396093'),
+        ('20', '3', 'acb359dc9b1bcff7e0f1965baa9f3f3dc18eeae99c49f1103c1e2986c0bbeed8'),
+        ('20', '4', 'fd93c74f5ec80689023648cdd12b2c5fb21a3898c81ebc3fa256ef244932562a'),
+        ('20', '5', '8e46d5be28c002f560050a71f4851b01ef45a3eb66ac90d7e23553fae1370e68'),
+        ('20', '6', '8cf9707b3d08ad9242326f02d1861831ad782c9bfb0c46e7b1f0d4640571d5c1'),
+        ('20', '7', '143b6a09f774aeae8b002afffb00839212020139a11873a3a1a34d4a63fa995d'),
+        ('20', '8', 'a6fc6d5c8ba0aad3a8afe44d1539cc299ef78ab53721e28244198fd5425d14ad'),
+        ('20', '9', '5ce6b534bab869b1e9bfefa353d7f578750e54fa72c8c9d74ddf129d993e78cf'),
         ('18', '1', '3cefac9a24ece99176d5d2d58fea2722de3e235be5138a128428b9260fe922ad'),
         ('18', '2', '3a0707a9a59dcbffa765dcf87b68001450095c51b96ec39d21260ba548a2f66a'),
         ('18', '3', '24c2e06f71ae553a408caa3f722254db2cbf1ca4db274542302184e3d6ca7015'),
@@ -92,17 +101,17 @@ class Amber(Package, CudaPackage):
               sha256=checksum, level=0, when='@{0}'.format(ver))
 
     # Patch to add ppc64le in config.guess
-    patch('ppc64le.patch', when='@18.20')
+    patch('ppc64le.patch', when='@18: target=ppc64le:')
 
     # Patch to add aarch64 in config.guess
-    patch('aarch64.patch', when='@18.20')
+    patch('aarch64.patch', when='@18: target=aarch64:')
 
     # Workaround to modify the AmberTools script when using the NVIDIA
     # compilers
-    patch('nvhpc.patch', when='@18.20 %nvhpc')
+    patch('nvhpc.patch', when='@18: %nvhpc')
 
     # Workaround to use NVIDIA compilers to build the bundled Boost
-    patch('nvhpc-boost.patch', when='@18.20 %nvhpc')
+    patch('nvhpc-boost.patch', when='@18: %nvhpc')
 
     variant('mpi', description='Build MPI executables',
             default=True)
@@ -113,17 +122,27 @@ class Amber(Package, CudaPackage):
     variant('update', description='Update the sources prior compilation',
             default=False)
 
+    # BOZ constant patch for gfortran10
+    patch('amber20_gcc10_boz.patch', when="@20: %gcc@10.1.0:")
+
+    depends_on('cmake', type='build')
+
     depends_on('zlib')
     depends_on('bzip2')
     depends_on('flex', type='build')
     depends_on('bison', type='build')
+    depends_on('netcdf-c')
     depends_on('netcdf-fortran')
+    depends_on('fftw')
+    depends_on('blas')
+    depends_on('lapack')
     # Potential issues with openmpi 4
     # (http://archive.ambermd.org/201908/0105.html)
     depends_on('mpi', when='+mpi')
 
     # Cuda dependencies
-    depends_on('cuda@:10.2.89', when='@18:+cuda')
+    depends_on('cuda@:11.9.99', when='@20:+cuda')
+    depends_on('cuda@:10.2.89', when='@18:19+cuda')
     depends_on('cuda@7.5.18', when='@:16+cuda')
 
     # conflicts
@@ -135,6 +154,13 @@ class Amber(Package, CudaPackage):
               msg='OpenMP not available for the Apple clang compiler')
     conflicts('+openmp', when='%pgi',
               msg='OpenMP not available for the pgi compiler')
+
+
+    @run_before('cmake')
+    def move_ambertools(self):
+        install_tree('ambertools_tmpdir', '.')
+        shutil.rmtree(join_path(self.stage.source_path, 'ambertools_tmpdir'))
+
 
     def setup_build_environment(self, env):
         amber_src = self.stage.source_path
@@ -149,77 +175,42 @@ class Amber(Package, CudaPackage):
         if self.spec.satisfies('+cuda'):
             env.set('CUDA_HOME', self.spec['cuda'].prefix)
 
-    def install(self, spec, prefix):
-        # The resource command does not allow us to expand the package in the
-        # root stage folder as required, as it already contains files. Here we
-        # install AmberTools where it should be, which results in 3 copies of
-        # the  ambertools (~9 GB). This has to be improved in the future.
-        install_tree('ambertools_tmpdir', '.')
-        shutil.rmtree(join_path(self.stage.source_path, 'ambertools_tmpdir'))
+    def cmake_args(self):
+
+        spec = self.spec
 
         # Select compiler style
-        if self.spec.satisfies('%cce'):
-            compiler = 'cray'
-        elif self.spec.satisfies('%gcc'):
-            compiler = 'gnu'
-        elif self.spec.satisfies('%intel'):
-            compiler = 'intel'
-        elif self.spec.satisfies('%pgi'):
-            compiler = 'pgi'
-        elif self.spec.satisfies('%nvhpc'):
-            compiler = 'pgi'
-        elif self.spec.satisfies('%clang'):
-            compiler = 'clang'
+        if spec.satisfies('%cce'):
+            compiler = 'CRAY'
+        elif spec.satisfies('%gcc'):
+            compiler = 'GNU'
+        elif spec.satisfies('%intel'):
+            compiler = 'INTEL'
+        elif spec.satisfies('%pgi'):
+            compiler = 'PGI'
+        elif spec.satisfies('%nvhpc'):
+            compiler = 'PGI'
+        elif spec.satisfies('%clang'):
+            compiler = 'CLANG'
         else:
             raise InstallError('Unknown compiler, exiting!!!')
 
-        # Base configuration
-        conf = Executable('./configure')
-        base_args = ['--skip-python',
-                     '--with-netcdf', self.spec['netcdf-fortran'].prefix,
-                     ]
-        if self.spec.satisfies('~x11'):
-            base_args += ['-noX11']
+        cmake_args = ["-DDOWNLOAD_MINICONDA=FALSE",
+                      "-DBUILD_GUI=FALSE",
+                      "-DINSTALL_TESTS=TRUE",
+                      "-DBUILD_PYTHON=FALSE",
+                      "-DBLA_VENDOR=All",
+                      f"-DCOMPILER={compiler}",
+                      self.define_from_variant("CUDA", "cuda"),
+                      self.define_from_variant("MPI", "mpi"),
+                      self.define_from_variant("OPENMP", "openmp")]
 
-        # Update the sources: Apply all upstream patches
-        if self.spec.satisfies('+update'):
-            update = Executable('./update_amber')
-            update(*(['--update']))
-        else:
-            base_args += ['--no-updates']
+        # Amber uses non standard BOZ constants in many places
+        if compiler == 'GNU' and spec.satisfies('%gcc@10.1.0:'):
+            cmake_args.append('-DCMAKE_Fortran_FLAGS=-fallow-invalid-boz')
 
-        # Non-x86 architecture
-        if self.spec.target.family != 'x86_64':
-            base_args += ['-nosse']
+        return cmake_args
 
-        # Single core
-        conf(*(base_args + [compiler]))
-        make('install')
-
-        # CUDA
-        if self.spec.satisfies('+cuda'):
-            conf(*(base_args + ['-cuda', compiler]))
-            make('install')
-
-        # MPI
-        if self.spec.satisfies('+mpi'):
-            conf(*(base_args + ['-mpi', compiler]))
-            make('install')
-
-        # Openmp
-        if self.spec.satisfies('+openmp'):
-            make('clean')
-            conf(*(base_args + ['-openmp', compiler]))
-            make('openmp')
-
-        # CUDA + MPI
-        if self.spec.satisfies('+cuda') and self.spec.satisfies('+mpi'):
-            make('clean')
-            conf(*(base_args + ['-cuda', '-mpi', compiler]))
-            make('install')
-
-        # just install everything that was built
-        install_tree('.', prefix)
 
     def setup_run_environment(self, env):
         env.set('AMBER_PREFIX', self.prefix)
